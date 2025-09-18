bf772dfe0369e32e1b1a7bc5b0abcee9a41d881d
PausedMagician <75096283+PausedMagician@users.noreply.github.com>
2025-09-18 12:00:56 +0200

    Created navigation with about and home

--- App.tsx
+++ App.tsx
@@ -1,13 +1,37 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
+import { createStaticNavigation, useNavigation } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-export default function App() {
+import './Screens/About';
+
+import React from 'react';
+import AboutPage from './Screens/About';
+
+function HomeScreen() {
+  const navigation = useNavigation();
+
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
+      <Button title='Do something' onPress={() => navigation.navigate('About')} />
       <StatusBar style="auto" />
     </View>
   );
+}
+
+const RootStack = createNativeStackNavigator({
+  initialRouteName: 'Home',
+  screens: {
+    Home: HomeScreen,
+    About: AboutPage
+  },
+});
+
+const Navigation = createStaticNavigation(RootStack);
+
+export default function App() {
+  return <Navigation />;
 }
 
 const styles = StyleSheet.create({
