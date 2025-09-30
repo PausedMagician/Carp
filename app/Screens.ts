91d8f80afed330ef91529bfc7e5b09858aad2d0c
PausedMagician <75096283+PausedMagician@users.noreply.github.com>
2025-09-30 15:52:44 +0200

    Implement login
    Context
    Hook
    Login page
    Prevents anything until login
    Setup ready for register page

--- app/Screens.ts
+++ app/Screens.ts
@@ -1,5 +1,6 @@
 import HomeScreen from "./Home";
 import VehiclesScreen from "./vehicles/VehiclesScreen";
+import LoginScreen from "./login/Login";
 
 // Export all screens
-export { HomeScreen, VehiclesScreen as VehicleScreen };
+export { HomeScreen, VehiclesScreen as VehicleScreen, LoginScreen };
