16da6f9b90e52bf673548f8d96aa2b222be45bde
Mikkel <mikkel@itnerd.dk>
2025-10-02 17:31:25 +0200

    Update VehicleController.ts

--- backend/controllers/VehicleController.ts
+++ backend/controllers/VehicleController.ts
@@ -4,27 +4,27 @@ import { Request, Response } from 'express';
 
 const vehicleRepository = AppDataSource.getRepository(Vehicle);
 
-export const createEmployee = async (req: Request, res: Response) => {
+export const createVehicle = async (req: Request, res: Response) => {
     const obj = vehicleRepository.create(req.body);
     await vehicleRepository.insert(obj);
     res.status(201).json(obj);
 };
 
-export const getEmployees = async (req: Request, res: Response) => {
+export const getVehicles = async (req: Request, res: Response) => {
     res.json(await vehicleRepository.find());
 };
 
-export const getEmployee = (req: Request, res: Response) => {
+export const getVehicle = (req: Request, res: Response) => {
     res.json(vehicleRepository.findOneBy({id: parseInt(req.params.id)}));
 };
 
-export const updateEmployee = async (req: Request, res: Response) => {
+export const updateVehicle = async (req: Request, res: Response) => {
     const obj = vehicleRepository.create(req.body);
     await vehicleRepository.save(obj);
     res.status(200).json(obj);
 };
 
-export const deleteEmployee = (req: Request, res: Response) => {
+export const deleteVehicle = (req: Request, res: Response) => {
     vehicleRepository.delete(req.body);
     res.status(200).json();
 };
