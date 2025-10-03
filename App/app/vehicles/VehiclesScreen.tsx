a35f3938a052768d6e41737f7e55c57be6256817
MV <mving23@student.sdu.dk>
2025-10-03 15:25:14 +0200

    Initial refactoring of loadVehicles function

--- App/app/vehicles/VehiclesScreen.tsx
+++ App/app/vehicles/VehiclesScreen.tsx
@@ -1,41 +1,75 @@
-import { styles } from "@/App";
-import { client } from "@/backend/Server";
-import CarListItem from "@/components/CarListItem";
-import { Vehicle } from "@/types/openapi";
-// import { Car, getAll } from "@/types/Car";
-import { StatusBar } from "expo-status-bar";
-import { useCallback, useEffect, useState } from "react";
-import { View, Text, FlatList } from "react-native";
+import {StatusBar} from "expo-status-bar";
+import {useCallback, useEffect, useState} from "react";
+import {View, Text, FlatList} from "react-native";
 
+import {styles} from "@/App";
+import {client} from "@/backend/Server";
+import CarListItem from "@/components/CarListItem";
+import {Vehicle} from "@/types/openapi";
+// import { Car, getAll } from "@/types/Car";
+
+interface VehicleWithAvailability {
+    car: Vehicle;
+    isAvailable: boolean;
+}
 
 export default function VehiclesScreen() {
-  const [cars, setCars] = useState<{car: Vehicle, isAvailable: boolean}[]>();
+    const [vehicles, setVehicles] = useState<VehicleWithAvailability[]>([]);
 
-  // const loadCars = useCallback(async () => {
-  //   try {
-      
-  //   }  
-  // });
+    // I imagine the useBooking() should be imported from BookingContext
+    // @ts-ignore
+    const {checkAvailability} = useBooking();
+
+    useEffect(() => {
+        loadVehicles();
+    }, []);
+
+    /**
+     * ToDo: Load vehicles from backend and check their availability
+     */
+    const loadVehicles = useCallback(async () => {
+        try {
+
+            // ToDo: Fetch vehicles from backend
 
 
-  useEffect(() => {
-    const map: {car: Vehicle, isAvailable: boolean}[] = [];
-    client.then(c => c.getVehicles().then(vehicles => {
-      vehicles.data.forEach(v => map.push({car: v, isAvailable: true})); // TODO: isAvailable
-      setCars(map);
-    })).catch(err => console.error(err));
-  }, [])
+            // ToDo: Check availability for each vehicle
+            // We could set a default as today and tomorrow as default date range
+            const today = new Date();
+            const tomorrow = new Date(today);
+            tomorrow.setDate(tomorrow.getDate() + 1);
 
-  return (
-    <View style={styles.container}>
-      <Text>Meow</Text>
-      {/* <Text>Cars: {cars.toString()}</Text> */}
-      <FlatList
-        data={cars}
-        renderItem={CarListItem}
-      />
-      <StatusBar style="auto" />
-      {/* <Navigator /> */}
-    </View>
-  );
+            // Collection consisting of available vehicles, make a Promise?
+            // @ts-ignore
+            const vehiclesWithAvailability = await Promise.all(
+
+            );
+
+            //setVehicles(vehiclesWithAvailability);
+        } catch (e) {
+            console.error("Error loading vehicles:", e);
+        }
+    }, [checkAvailability]);
+
+    // --- Old ---
+    useEffect(() => {
+        const map: { car: Vehicle, isAvailable: boolean }[] = [];
+        client.then(c => c.getVehicles().then(vehicles => {
+            vehicles.data.forEach(v => map.push({car: v, isAvailable: true})); // TODO: isAvailable
+            setVehicles(map);
+        })).catch(err => console.error(err));
+    }, [])
+
+    return (
+        <View style={styles.container}>
+            <Text>Meow</Text>
+            {/* <Text>Cars: {cars.toString()}</Text> */}
+            <FlatList
+                data={vehicles}
+                renderItem={CarListItem}
+            />
+            <StatusBar style="auto"/>
+            {/* <Navigator /> */}
+        </View>
+    );
 }
