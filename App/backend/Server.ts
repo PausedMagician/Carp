3cf24d39650254a342b2786dfe80a6dd565eb9f9
Mikkel <mikkel@itnerd.dk>
2025-10-05 19:07:18 +0200

    Update Server.ts

--- App/backend/Server.ts
+++ App/backend/Server.ts
@@ -7,7 +7,7 @@ import { Platform } from 'react-native';
 // 
 // If the line below have been modified, to a local environment, the cloud environment will fail!
 //
-const ENDPOINT = Platform.OS === 'web' ? "https://carp-app.vm1.itnerd.dk/api" : "your ip endpoint";
+const ENDPOINT = Platform.OS === 'web' ? "https://carp-app-api.vm1.itnerd.dk" : "your ip endpoint";
 const api = new OpenAPIClientAxios({ 
   definition: `${ENDPOINT}/v1.json`,
   axiosConfigDefaults: {
