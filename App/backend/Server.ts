980a4d402c41bf0aaf1acbe08de0e7b077fbc739
MV <mving23@student.sdu.dk>
2025-10-05 19:11:56 +0200

    Applying Server.ts fix

--- App/backend/Server.ts
+++ App/backend/Server.ts
@@ -2,28 +2,25 @@ import { Client } from '@/types/openapi'
 import OpenAPIClientAxios from 'openapi-client-axios';
 import { Platform } from 'react-native';
 
-// // OBS: Pleasse check this section before committing!!
-// //
-// // If the line below have been modified to a local environment, the cloud environment will fail!
+// OBS: Pleasse check this section before committing!!
 //
-// const ENDPOINT = Platform.OS === 'web' ? "localhost" : "your ip endpoint";
-// const api = new OpenAPIClientAxios({
-//   definition: `${ENDPOINT}/v1.json`,
-//   axiosConfigDefaults: {
-//     baseURL: `${ENDPOINT}/`
-//   }
-// });
-//
-// export const client: Promise<Client> = api.init<Client>();
-
-const HOST = Platform.OS === 'web' ? "localhost" : "your ip address";
-const PORT = 3000;
-
+// If the line below have been modified to a local environment, the cloud environment will fail!
+const ENDPOINT = Platform.OS === 'web' ? "https://carp-app-api.vm1.itnerd.dk" : "your ip endpoint";
 const api = new OpenAPIClientAxios({
-    definition: `http://${HOST}:${PORT}/v1.json`,
-    axiosConfigDefaults: {
-        baseURL: `http://${HOST}:${PORT}/`
-    }
+  definition: `${ENDPOINT}/v1.json`,
+  axiosConfigDefaults: {
+    baseURL: `${ENDPOINT}/`
+  }
 });
 
 export const client: Promise<Client> = api.init<Client>();
+
+// const HOST = Platform.OS === 'web' ? "localhost" : "your ip address";
+// const PORT = 3000;
+//
+// const api = new OpenAPIClientAxios({
+//     definition: `http://${HOST}:${PORT}/v1.json`,
+//     axiosConfigDefaults: {
+//         baseURL: `http://${HOST}:${PORT}/`
+//     }
+// });
